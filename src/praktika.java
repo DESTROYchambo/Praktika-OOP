a8cd4f9318b0fec05a9fcbb339bfbca071c56ab1
OleksandrNarikov <148896479+DESTROYchambo@users.noreply.github.com>
2025-03-24 20:31:16 +0200

    Create praktika.java

---
+++ src/praktika.java
@@ -0,0 +1,11 @@
+public class praktika {
+
+public static void main(String[] args) {
+       System.out.println("1 zavd ");
+       for(int i=0;i<args.length;i++){
+           System.out.println(args[i]);
+
+       }
+    }
+    
+}
